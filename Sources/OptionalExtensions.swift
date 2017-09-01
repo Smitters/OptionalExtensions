40f995d8841a7c7a23e36d55b19ce3be1330af66
Dmitry Smetankin <smetankin93@gmail.com>
2017-09-01 22:42:40 +0300

    swiftplate init

---
+++ Sources/OptionalExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  OptionalExtensions.swift
+//  OptionalExtensions
+//
+//  Created by Dmitry Smetankin on 01.09.17.
+//  Copyright © 2017 OptionalExtensions. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/OptionalExtensionsTests/OptionalExtensionsTests.swift
@@ -0,0 +1,23 @@
+//
+//  OptionalExtensionsTests.swift
+//  OptionalExtensions
+//
+//  Created by Dmitry Smetankin on 01.09.17.
+//  Copyright © 2017 OptionalExtensions. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import OptionalExtensions
+
+class OptionalExtensionsTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(OptionalExtensions().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
